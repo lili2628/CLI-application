1ca0d537b6bc56aa37fa6e46fce41b39cc87d440
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-06-30 19:56:00 +0300

    update

---
+++ index.js
@@ -0,0 +1,42 @@
+//const argv = require("yargs").argv;
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            const allContacts = await contacts.listContacts();
+            console.table(allContacts);
+        break;
+
+        case 'get':
+            const oneContact = await contacts.getContactById(id);
+            console.log(oneContact);
+        break;
+
+        case 'add':
+            const newContact = await contacts.addContact({ name, email, phone });
+            console.log(newContact);
+        break;
+
+        case 'remove':
+            const deletedContact = await contacts.removeContact(id);
+            console.log(deletedContact);
+        break;
+
+        default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
